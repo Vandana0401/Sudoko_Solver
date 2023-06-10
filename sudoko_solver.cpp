d8c0f3435e9a0710f3aaa79a0932c7ca60e5ba7d
Vandana0401 <136062766+Vandana0401@users.noreply.github.com>
2023-06-10 15:04:16 +0530

    first commit

---
+++ sudoko_solver.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define N 9 
+
+bool isSafe(vector<vector<int>>&board,int row, int col, int num) 
+    { 
+     
+        for (int d = 0; d < N; d++)  
+        { 
+            
+            if (board[row][d] == num) { 
+                return false; 
+            } 
+        } 
+  
+        for (int r = 0; r < N; r++)  
+        { 
+               
+            if (board[r][col] == num)  
+            { 
+                return false; 
+            } 
+        }  
+        int s = (int)sqrt(N); 
+        int boxRowStart = row - row % s; 
+        int boxColStart = col - col % s; 
+  
+        for (int r = boxRowStart; 
+             r < boxRowStart + s; r++)  
+        { 
+            for (int d = boxColStart; 
+                 d < boxColStart + s; d++)  
+            { 
+                if (board[r][d] == num)  
+                { 
+                    return false; 
+                } 
+            } 
+        } 
+  
+        return true; 
+    } 
+
+bool solve(vector<vector<int>>&board,int n) 
+{ 
+	    int row = -1; 
+        int col = -1; 
+        bool isEmpty = true; 
+        for (int i = 0; i < n; i++)  
+        { 
+            for (int j = 0; j < n; j++)  
+            { 
+                if (board[i][j] == 0)  
+                { 
+                    row = i; 
+                    col = j; 
+                    isEmpty = false; 
+                    break; 
+                } 
+            } 
+            if (!isEmpty) { 
+                break; 
+            } 
+        } 
+  
+        if (isEmpty)  
+        { 
+            return true; 
+        } 
+        
+        for (int num = 1; num <= n; num++)  
+        { 
+            if (isSafe(board, row, col, num))  
+            { 
+                board[row][col] = num; 
+                if (solve(board, n))  
+                { 
+                    // print(board, n); 
+                    return true; 
+                } 
+                else 
+                { 
+                    board[row][col] = 0; 
+                } 
+            } 
+        } 
+        return false; 
+} 
+
+void printGrid(vector<vector<int>>&grid) 
+{ 
+    cout<<endl;
+    cout<<"Your Solution is ...."<<endl;
+    cout<<endl;
+    
+	for (int row = 0; row < N; row++) 
+	{ 
+		for (int col = 0; col < N; col++) 
+			cout << grid[row][col] << " "; 
+		cout << endl; 
+	} 
+} 
+ 
+int main() 
+{ 
+    
+    
+    int gridlenght;
+    
+    cout<<"Enter the sudoko puzzle of 9 x 9 size"<<endl;
+    
+    vector<vector<int>>grid(9,vector<int>(9,0));
+    
+    for(int i=0;i<9;i++)
+    {
+        for(int j=0;j<9;j++)
+        {
+            cin>>grid[i][j];
+        }
+    }
+    
+    
+// 	example ::    { { 3, 0, 6, 5, 0, 8, 4, 0, 0 }, 
+// 					{ 4, 2, 0, 0, 0, 0, 0, 0, 0 }, 
+// 					{ 0, 8, 7, 0, 0, 0, 0, 3, 1 }, 
+// 					{ 0, 0, 3, 0, 1, 0, 0, 8, 0 }, 
+// 					{ 9, 0, 0, 8, 6, 3, 0, 0, 5 }, 
+// 					{ 0, 5, 0, 0, 9, 0, 6, 0, 0 }, 
+// 					{ 1, 3, 0, 0, 0, 0, 2, 5, 0 }, 
+// 					{ 0, 0, 0, 0, 0, 0, 0, 7, 4 }, 
+// 					{ 0, 0, 5, 2, 0, 6, 3, 0, 0 } }; 
+
+
+	if (solve(grid,N) == true) 
+		printGrid(grid); 
+	else
+	{
+	    cout<<endl;
+	    cout << "No solution exists"; 
+	}
+		
+
+	return 0; 
+} 
+
+
+
